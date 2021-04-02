99d9bbe05291e7f6e87081cfa2119db2c80eb4d6
boa50 <bruno.albuquerque50@gmail.com>
2021-04-02 17:05:14 -0300

    Inclusão de código para imagens

--- imagens.py
+++ imagens.py
@@ -5,6 +5,14 @@ plt.imshow(image, cmap='gray')
 plt.savefig('nome_arquivo.png')
 
 
+# Salva uma imagem gerada pelo matplotlib sem bordas em disco
+import matplotlib.pyplot as plt
+
+plt.imshow(image)
+plt.axis('off')
+plt.savefig('nome_arquivo.png', bbox_inches='tight',pad_inches = 0)
+
+
 # Cria um gif a partir de um conjunto de imagens salvas em disco
 import glob
 import imageio
